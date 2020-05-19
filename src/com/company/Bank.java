56ad80133109bd47a17ea8b43ea0d6237a28eab8
hussamAdil <hussam0683@gmail.com>
2020-05-19 15:27:32 +0200

    connected to mysql + include mysql connector jar file

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,12 +1,34 @@
 package com.company;
 
+import java.sql.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class  Bank {
-    private String name ;
-    public Bank(String name )
+    private String name;
+    public Scanner scanner = new Scanner(System.in);
+
+    String connectionUrl = "jdbc:mysql://127.0.0.1:3306/bankCustomer";
+
+    Connection dbconnection;
+
     {
+        try {
+            dbconnection = DriverManager.getConnection(connectionUrl, "root", "");
+            // create a statement object to send to database
+            Statement statement = dbconnection.createStatement();
+            ResultSet result = statement.executeQuery("select * from customer");
+            while (result.next()) {
+                System.out.println(result.getString("name"));
+            }
+        } catch (SQLException throwables) {
+            throwables.getMessage();
+        }
+    }
+
+
+
+    public Bank(String name )  {
         setName(name);
     }
 
@@ -19,10 +41,9 @@ public class  Bank {
     }
     public void createAccount()
     {
-        Scanner scanner = new Scanner(System.in);
-        try {
+         try {
             //  int id , String name , String address , String phone,int balance
-            System.out.println("Enter account  Id ? ");
+             System.out.println("Enter account  Id ? ");
             int id = scanner.nextInt();
             System.out.println("Enter customer name  ? ");
             String name = scanner.next();
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,37 @@
 package com.company;
+
+import java.sql.*;
+
 public class Main<control> {
-/*
-    To-Do List :
-    - create file to save data
-    - search over file
- */
+
     public static void main(String[] args) {
-        // Show Welcoming screen
-        ConsoleController.showMenu();
-        // Get what customer need to Throws Exception
-        int option =  ConsoleController.getOption();
-        // Create  Your Bank
-        Bank KhartoumBank = new Bank("Khartoum") ;
-        // Run Bank all operations depend on what customer Need
-        KhartoumBank.operations(option);
+//        // Show Welcoming screen
+//        ConsoleController.showMenu();
+//        // Get what customer need to Throws Exception
+//        int option =  ConsoleController.getOption();
+//        // Create  Your Bank
+//        Bank KhartoumBank = new Bank("Khartoum") ;
+//        // Run Bank all operations depend on what customer Need
+//        KhartoumBank.operations(option);
+
+        String connectionUrl = "jdbc:mysql://127.0.0.1:3306/bankCustomer";
+
+        Connection dbconnection;
+
+        {
+            try {
+                dbconnection = DriverManager.getConnection(connectionUrl, "root", "");
+                // create a statement object to send to database
+                Statement statement = dbconnection.createStatement();
+                ResultSet result = statement.executeQuery(" select * from customer ");
+                while (result.next()) {
+                  System.out.println(result.getString("name"));
+                }
+            } catch (SQLException throwables) {
+                throwables.getStackTrace();
+            }
+        }
+
+
     }
 }
